fce10f7a223590c95fcde6895984c689ce9e0938
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-08-14 22:31:49 +0200

    backend done

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -15,7 +15,8 @@ import { usePathname, useRouter } from "next/navigation";
 import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from '@hookform/resolvers/zod';
 // import { updatedUser } from "@/lib/actions/user.actions";
-import { ThreadValidation } from "@/lib/validations/user";
+import { ThreadValidation } from '@/lib/validations/thread';
+import { createThread } from '@/lib/actions/thread.action';
 
 // ts interface
 interface Props {
@@ -43,8 +44,16 @@ function PostThread({ userId }: { userId: string }) {
         }
     });
 
-    const onSubmit = () => {
-        // await createThreads();
+    const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+        await createThread({
+            text: values.thread,
+            author: userId,
+            communityId: null,
+            path: pathname
+
+        });
+
+        router.push('/');
         
     }
     return (
